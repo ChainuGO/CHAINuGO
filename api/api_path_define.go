7d26e6919cc29fec621d8b7e88552c4bf548d249
chainugo <chainugo@gmail.com>
2025-03-31 20:08:30 +0800

    Add files via upload

---
+++ api/api_path_define.go
@@ -0,0 +1,10 @@
+package api
+
+const (
+	MainNetEndpoint = "https://vapi.ChainuGO.com/sdk"
+	DevNetEndpoint  = "https://testnet.ChainuGO.com/sdk"
+
+	PathCreateUser           = "/user/create"
+	PathCreateWallet         = "/wallet/create"
+	PathUserWithdrawByOpenID = "/partner/UserWithdrawByOpenID"
+)
